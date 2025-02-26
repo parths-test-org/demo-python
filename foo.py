54f46374aaa8ec9f3b0b3f084f8c922574646b2a
Parth <86726240+parth-deepsource@users.noreply.github.com>
2025-02-26 12:32:36 +0530

    more changes

--- foo.py
+++ foo.py
@@ -7,35 +7,42 @@ b = 2
 assert a == 1
 print(b, nums)
 
+
 def baz(a=[]):
-  return 0
+    return 0
+
 
 def aaa(a=[]):
-  return 1
+    return 1
 
 
 def foo(b=[]):
-  return 1
+    return 1
+
 
 def bar(a):
-  return 1
+    return 1
+
 
 import os
+
 filename = os.tmpnam()
-with open(filename, 'w') as f:
-  pass
+with open(filename, "w") as f:
+    pass
+
 
 def boom(a=[]):
-  breakpoint()
-  filename = os.tmpnam()
-  breakpoint()
-  return filename
+    breakpoint()
+    filename = os.tmpnam()
+    breakpoint()
+    return filename
+
 
 def another_test_method():
-  f = open("/tmp/.deepsource.toml", "r")
-  f.write("config file.")
-  f.close()
-  print('abc')
-  
-filename = os.tmpnam()
+    f = open("/tmp/.deepsource.toml", "r")
+    f.write("config file.")
+    f.close()
+    print("abc")
 
+
+filename = os.tmpnam()
